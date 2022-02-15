dc40ff9153f1eb828556d254fd4d774cf5958e38
YotsuyaSan <zaccaria.maldrie@outlook.com>
2022-02-15 03:37:19 +0100

    default

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import { ChakraProvider } from '@chakra-ui/react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './App.css';
+import Home from './Pages/Home';
+
+function App() {
+  return <ChakraProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+        </Routes>
+      </BrowserRouter>
+  </ChakraProvider>
+}
+
+export default App;
---
+++ src/Pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {VStack} from '@chakra-ui/layout'
+
+const Home = () => {
+  return (
+    <VStack>
+      
+    </VStack>
+    )
+}
+
+export default Home
