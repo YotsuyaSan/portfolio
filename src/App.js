771fdff8addc9cdd3e36edbb879eee0ba9024c92
YotsuyaSan <zaccaria.maldrie@outlook.com>
2022-02-22 21:36:25 +0100

    aa

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { ChakraProvider, ColorModeScript } from '@chakra-ui/react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Fonts from './Components/Fonts';
 import Navbar from './Components/Navbar';
 import Contact from './Pages/Contact';
 import Home from './Pages/Home';
@@ -9,19 +10,22 @@ import Stages from './Pages/Stages';
 import Veille from './Pages/Veille';
 
 function App() {
-  return <ChakraProvider>
-    <ColorModeScript initialColorMode='light'></ColorModeScript>
-    <Navbar/>
+  return (
+    <ChakraProvider>
+      <ColorModeScript initialColorMode='light'/>
+      <Navbar />
       <BrowserRouter>
+      
         <Routes>
-          <Route path="/" element={<Home/>}/>
-          <Route path="/competences" element={<Skills/>}/>
-          <Route path="/stages" element={<Stages/>}/>
-          <Route path="/veille" element={<Veille/>}/>
-          <Route path="/contact" element={<Contact/>}/>
+          <Route path="/" element={<Home />} />
+          <Route path="/competences" element={<Skills />} />
+          <Route path="/stages" element={<Stages />} />
+          <Route path="/veille" element={<Veille />} />
+          <Route path="/contact" element={<Contact />} />
         </Routes>
       </BrowserRouter>
-  </ChakraProvider>
+    </ChakraProvider>
+  )
 }
 
 export default App;
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import { Heading, Box, Text, VStack, Flex, Stack, Container } from '@chakra-ui/react'
 
-const Section = ({title, desc}) => {
-  return(
+const Section = ({ title, desc }) => {
+  return (
     <Box p={5}>
       <Heading fontSize="xl">{title}</Heading>
       <Text mt={4}>{desc}</Text>
@@ -16,17 +16,17 @@ const Home = () => {
       <Flex w="100%">
         <Container maxW="container.lg" wrap="wrap">
           <Stack>
-            <Heading fontSize="xl">Diplômes et formations</Heading>
-            <Text>Lycée dampierre Valenciennes</Text>
+            <Heading as="h3" variant="section-title" fontSize="2xl">Diplômes et formations</Heading>
+            <Text fontSize="lg">Lycée dampierre Valenciennes</Text>
             <Text>BTS Services Informatiques aux Organisations</Text>
 
             <Text>Lycée professionnel St Luc Cambrai</Text>
-            <Text>Bac professionnel Systèmes Numériques option Réseaux Informatiques et Systèmes Communiquantss</Text>
+            <Text>Bac professionnel Systèmes Numériques option Réseaux Informatiques et Systèmes Communiquants</Text>
           </Stack>
         </Container>
       </Flex>
     </VStack>
-    )
+  )
 }
 
 export default Home
