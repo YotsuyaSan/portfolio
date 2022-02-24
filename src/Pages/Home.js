6992c943d64b5e473927145798c49ae6f014ba7e
YotsuyaSan <zaccaria.maldrie@outlook.com>
2022-02-24 01:17:36 +0100

    1

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -9,7 +9,7 @@ const Home = () => {
           <Stack>
             <Heading variant="section-title">À propos de moi</Heading>
             <Box p={4} borderWidth={2}>
-              <Text></Text>
+              <Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec volutpat arcu at maximus consectetur. Proin tincidunt elementum diam eu volutpat. Nam malesuada, justo in gravida laoreet, purus ipsum auctor ipsum, sit amet mattis velit libero a purus. Sed ultrices urna nulla, in porttitor neque aliquam quis. Etiam ullamcorper faucibus lectus, et condimentum nunc gravida non. Pellentesque facilisis dui turpis, non tincidunt augue vehicula et. Nulla luctus auctor metus, ut pellentesque urna. Pellentesque a lectus vitae nisi lobortis varius sit amet id purus. Etiam ante neque, aliquam ornare semper a, semper efficitur quam. In tempor ac nisl et fermentum. In semper, ante at fermentum faucibus, turpis velit commodo eros, eget vehicula lorem felis quis nibh. Cras leo ligula, volutpat nec imperdiet id, laoreet id diam. </Text>
             </Box>
             <Heading variant="section-title">Parcours scolaire</Heading>
             <br />
--- src/Pages/Skills.js
+++ src/Pages/Skills.js
@@ -1,32 +1,32 @@
-import { CircularProgress, CircularProgressLabel, Container, Flex, Heading, Text, VStack, Stack } from '@chakra-ui/react'
+import { CircularProgress, CircularProgressLabel, Container, Flex, Heading, Text, VStack, Stack, Spacer } from '@chakra-ui/react'
 import React from 'react'
 
 const Skills = () => {
   return (
     <VStack p={5}>
-      <Flex w="100%" bg="black">
-        <Container bg="red" maxW="container.lg" wrap="wrap">
+      <Flex w="100%">
+        <Container maxW="container.lg" wrap="wrap">
           <Heading variant="section-title">Mes compétences</Heading>
           <Stack
             direction={{ base: "column", md: "row" }}
-            width={{ base: "full", md: "auto" }}
-            alignItems="center"
             spacing={6}
             flexgrow={1}
           >
-            <Container>
+            <Container p={4} textAlign='center'>
               <CircularProgress value={70} size="120px">
                 <CircularProgressLabel>70%</CircularProgressLabel>
               </CircularProgress>
-              <Text>Node.js & <br /> Express.js</Text>
+              <Text>Node.js & Express.js</Text>
             </Container>
-            <Container>
+            <Spacer/>
+            <Container p={4} textAlign="center">
               <CircularProgress value={70} size="120px">
                 <CircularProgressLabel>70%</CircularProgressLabel>
               </CircularProgress>
               <Text>React.js</Text>
             </Container>
-            <Container>
+            <Spacer/>
+            <Container p={4} textAlign="center">
               <CircularProgress value={70} size="120px">
                 <CircularProgressLabel>70%</CircularProgressLabel>
               </CircularProgress>
