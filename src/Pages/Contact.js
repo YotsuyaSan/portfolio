1523073bf6e31f8cc599115d3230512f1aeab69e
YotsuyaSan <zaccaria.maldrie@outlook.com>
2022-02-24 17:56:01 +0100

    i

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -1,10 +1,26 @@
-import { VStack } from '@chakra-ui/react'
+import { Container, Flex, Heading, VStack, Text, Box, Link, Button, Stack } from '@chakra-ui/react'
 import React from 'react'
+import { AiFillGithub, AiFillLinkedin } from 'react-icons/ai'
 
 const Contact = () => {
   return (
     <VStack p={5}>
-      
+      <Flex w="100%">
+        <Container maxW="container.lg" wrap="wrap">
+          <Heading variant="section-title">Contact</Heading>
+          <Box p={4} borderWidth={2}>
+            <Stack direction="column">
+              
+            <Link href='https://github.com/YotsuyaSan'>
+              <Button leftIcon={<AiFillGithub/>}>@YotsuyaSan</Button>
+            </Link>
+            <Link href='https://www.linkedin.com/in/zaccaria-maldrie-6623811b9/'>
+              <Button leftIcon={<AiFillLinkedin/>}>Maldrie Zaccaria</Button>
+            </Link>
+            </Stack>
+          </Box>
+        </Container>
+      </Flex>
     </VStack>
   )
 }
--- src/Pages/Stages.js
+++ src/Pages/Stages.js
@@ -1,8 +1,24 @@
+import { Box, Flex, Image, Link, Text, VStack } from '@chakra-ui/react'
 import React from 'react'
+import akao from '../assets/akao-life.png'
 
 const Stages = () => {
   return (
-    <div>Stages</div>
+    <VStack p={5}>
+      <Flex w="100%">
+        <Box maxW="15%">
+        <Link href='http://www.akaolife.fr/'>
+          <Image src={akao}/>
+          <Text>
+            Akaolife propose une solution fullweb, souple et personnalisable, répondant aux principales problématiques d'optimisation et de rationalisation de vos process internes.
+          </Text>
+          <Text> 
+            Akaolife vous permet d'optimiser la gestion des flux d'informations et de gagner ainsi, en temps, en efficacité, en organisation. 
+          </Text>
+        </Link>
+        </Box>
+      </Flex>
+    </VStack>
   )
 }
 
